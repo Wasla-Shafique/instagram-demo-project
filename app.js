a7b80cbf821d15e436ce8e028546e313cd72e07b
wasla-shafique <wasla.shafique@dubizzlelabs.com>
2023-11-30 00:49:51 +0500

    Add Sequelize migrations

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 // index.js
 const express = require('express');
-// const User = require('./models/User'); // Adjust the path based on your file structure
-// const Post = require('./models/Post'); // Adjust the path based on your file structure
-// const sequelize = require('./sequel-config');
+const User = require('./models/User'); // Adjust the path based on your file structure
+const Post = require('./models/Post'); // Adjust the path based on your file structure
+const sequelize = require('./sequel-config');
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sync models with the database
-// (async () => {
-//   try {
-//     await sequelize.sync();
-//     console.log("inside sql")
-//     console.log('Database synced successfully');
-//   } catch (error) {
-//     console.error('Error syncing database:', error);
-//   }
-// })();
+console.log("before async");
 
-// app.get('/', async (req, res) => {
-//   try {
-//     const users = await User.findAll();
-//     res.json(users);
-//   } catch (error) {
-//     console.error('Error fetching users:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
+(async () => {
+  try {
+    console.log("inside sql")
+    await sequelize.sync();
+    console.log('Database synced successfully');
+
+  } catch (error) {
+    console.error('Error syncing database:', error);
+  }})();
+
+console.log("after async");
+
+
+app.get('/', async (req, res) => {
+  try {
+    console.log("inside get :")
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
